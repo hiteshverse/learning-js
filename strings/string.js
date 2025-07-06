a02c679272c39591213db4933c6db4428542fd6c
Hitesh Kumar <hiteshkumar0778@gmail.com>
2025-07-06 15:04:42 +0530

    string methods added

--- strings/string.js
+++ strings/string.js
@@ -1,6 +1,20 @@
 // template literal or string interpolation in js
 
-let myName = "Hitesh";
-let myAge = 19;
+// let myName = "Hitesh";
+// let myAge = 19;
 
-console.log(`Hey there, My name is ${myName} and I'm ${myAge} years old.`);
+// console.log(`Hey there, My name is ${myName} and I'm ${myAge} years old.`);
+
+
+// String are immutable in js 
+let text = "something";
+// text[0] = 'h';
+// console.log(text)
+
+// we can't change the original string once declared 
+// but we can still get the value by using the square brackets just like arrays 
+
+console.log(text[0])
+
+// also a method 
+console.log(text.at(0))
--- strings/stringMethods.js
+++ strings/stringMethods.js
@@ -1,20 +1,43 @@
-let word = 'string methods';
+let str = "testing and learning all the and string methods."
+let str1 = "a * b * c * d * e * c * f"
 
-// length is the property for finding the length of any object in js
-// console.log(word.length);
+// since strings are immutable every string manipulations methods returns a new string in js 
 
-// console.log(word.toUpperCase());
-// console.log(word[5]);
-console.log(word.slice(2, 5));
-console.log(word.slice(6));
+// length is the property for finding length of a string, it's not a function 
 
-
-console.log(word.replace('string', 'array'));
-console.log(word.concat(" are", " defined", " here"));
-console.log(word + " are" + " defined" + " here");
-console.log("       hello               world                ".trim());
+console.log(str.length)
 
 
-let str = "hello";
-str[0] = 'f';
-console.log(str);
+// console.log(str.indexOf("and"))
+
+console.log(str.indexOf("s")) // return index for the first occurence of the substring
+console.log(str.lastIndexOf("s")) // returns the index for the last occurence of the substring
+
+console.log(str.slice(0, 6 +1 ))  // returns the sliced portion of a string 
+
+console.log(str.substring(0, 6 + 1))
+
+
+// since substring and slice do the same work but there's a major diff
+
+console.log(str.slice(0, 7)) 
+// if we add the first param greater than the second then it will just print and empty string 
+console.log(str.slice(7, 0))
+
+// but while using the substring method if the first param is greater than second then it will just swap the values
+
+console.log("__________")
+console.log(str.substring(0, 7))
+console.log(str.substring(7, 0))  // values swapped 
+
+
+console.log(str.replace("and", "checked")) // replaces the first occurence of a given char or string
+console.log(str.replaceAll("and", "checked")) // replaces all the substring with the replaces string
+
+console.log(str.split(" ")); // splits the string into substring by a given separator and return them as an array of substrings. 
+// console.log(str1.split("*"));
+
+console.log("__________")
+const testStr = "          helolworld        "
+console.log(testStr)
+console.log(testStr.trim()) // removes whitespaces frm
