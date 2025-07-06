51899c5c698add216b13d86d8ba3a3718f5cbc4d
Hitesh Kumar <hiteshkumar0778@gmail.com>
2025-07-06 15:12:19 +0530

    numbers methods added

---
+++ Numbers/numbers.js
@@ -0,0 +1,15 @@
+let abc = "hello";
+let num = Number.parseInt(abc);       // converts a string into number
+
+console.log(num)
+
+
+const one = "23434343.434";
+console.log(Number.parseInt(one) + ` ${typeof Number.parseInt(one)}  `)
+
+
+const two = "two";
+console.log(Number.parseFloat(two));
+
+const two1 = "3434343";
+console.log(Number.parseFloat(two1));       // converts a string into decimal value 
