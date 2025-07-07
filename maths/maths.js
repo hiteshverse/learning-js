195002890ae82ba658302f4c085c84d991765538
Hitesh Kumar <hiteshkumar0778@gmail.com>
2025-07-07 23:25:27 +0530

    maths functions added

---
+++ maths/maths.js
@@ -0,0 +1,46 @@
+// some methematical functions 
+
+
+// console.log(Math.sqrt(4));
+
+
+// diff in round, ceil and floor 
+// let num = 343.4;
+
+// console.log(Math.round(num))   // rounds to the nearest integer
+
+
+// console.log(Math.floor(num)) // removes the decimal part of a num
+
+// console.log(Math.ceil(num));
+
+// console.log(Math.ceil(0.1))
+
+// console.log(Math.ceil(-3.43))
+
+
+
+
+// console.log(Math.ceil(0.1))
+
+// console.log(Math.ceil(-3.43))
+// round -> rounds to the nearest int
+// floor -> rounds down to the nearest int toward negetive
+// ceil -> returns the smallest integer which is greater than or equal to the passed value 
+
+// console.log(Math.floor(-8.14))
+// console.log(Math.floor(8.14))
+
+
+// console.log(Math.floor(4.5));
+// console.log(Math.floor(-3.5));
+
+// console.log(Math.ceil(5.6));
+// console.log(Math.ceil(-34.5));
+
+// console.log(Math.round(3.4));
+// console.log(Math.round(3.5));
+
+console.log(Math.round(-3.5));
+// console.log(Math.round(-3.4));
+// console.log(Math.round(-3.6));
